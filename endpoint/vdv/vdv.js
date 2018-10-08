51248abd5d59ecf970c3282b4739141ed022bb5f
Alex Lardschneider <alex.lardschneider@gmail.com>
2018-10-08 20:01:23 +0200

    Provide default color for line in GTFS

--- endpoint/vdv/vdv.js
+++ endpoint/vdv/vdv.js
@@ -1375,7 +1375,7 @@ function appendRouteColors(client) {
                         firstLine = false;
                     } else {
                         let lineId = line.split(",")[1];
-                        let color = colors[lineId];
+                        let color = colors[lineId] || "FFFFFF";
 
                         logger.info(`Color for line ${lineId}: ${color}`);
 
